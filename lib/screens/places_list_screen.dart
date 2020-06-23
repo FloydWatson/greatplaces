f01b2b588cadf334c8bbf1f91a9b14fa016b43bf
FloydWatson <floydbcseit@gmail.com>
2020-06-23 23:25:42 +1200

    added sqflite to project. insert and fetch methods

--- lib/screens/places_list_screen.dart
+++ lib/screens/places_list_screen.dart
@@ -20,24 +20,38 @@ class PlacesListScreen extends StatelessWidget {
           ),
         ],
       ),
-      body: Consumer<GreatPlaces>(
-        child: Center(
-          child: Text('no places yet'),
-        ),
-        builder: (ctx, greatPlaces, ch) => greatPlaces.items.length <= 0
-            ? ch
-            : ListView.builder(
-                itemCount: greatPlaces.items.length,
-                itemBuilder: (ctx, i) => ListTile(
-                  leading: CircleAvatar(
-                    backgroundImage: FileImage(greatPlaces.items[i].image),
-                    
-                  ),
-                  title: Text(greatPlaces.items[i].title),
-                  onTap: () {
-                    // go to detail page here
-                  },
+      // point at the future and build when future has been returned.
+      // listen false so build method isnt re ran when future returned. would enter reload loop
+      body: FutureBuilder(
+        future: Provider.of<GreatPlaces>(context, listen: false)
+            .fetchAndSetPlaces(),
+        builder: (ctx, snapshot) => snapshot.connectionState ==
+                ConnectionState.waiting // if still loading
+            ? Center(
+                child: CircularProgressIndicator(),
+              )
+              // consumer so it is updated as list is updated. This needs to listen to provider
+            : Consumer<GreatPlaces>(
+                child: Center(
+                  child: const Text('Got no places yet, start adding some!'),
                 ),
+                // ch is the above child. We will run this child if list is empty
+                builder: (ctx, greatPlaces, ch) => greatPlaces.items.length <= 0
+                    ? ch
+                    : ListView.builder(
+                        itemCount: greatPlaces.items.length,
+                        itemBuilder: (ctx, i) => ListTile(
+                          leading: CircleAvatar(
+                            backgroundImage: FileImage(
+                              greatPlaces.items[i].image,
+                            ),
+                          ),
+                          title: Text(greatPlaces.items[i].title),
+                          onTap: () {
+                            // Go to detail page ...
+                          },
+                        ),
+                      ),
               ),
       ),
     );
